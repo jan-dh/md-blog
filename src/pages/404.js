259f7ddae4abb711800633c8388cbd25acaeb599
Jan <jan@glue.be>
2018-07-14 17:08:39 +0200

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+import Layout from '../components/layout'
 
-export default NotFoundPage
+class NotFound extends React.Component {
+  render() {
+    const posts = this.props.data.allMarkdownRemark.edges
+    return (
+      <Layout location={this.props.location}>
+      <div className="mx-auto max-w-sm pt-12 pb-8">
+        <h1>Page not found</h1>
+        <p className="my-4">This is not the page you are looking for.</p>
+        <img src="/404.gif" alt="404"/>
+       {posts.map(({node}) => {
+          return (
+            <div className="my-4">
+            <p className="my-4">Maybe you were looking for one of these posts?</p>
+              <Link to={node.fields.slug}  className="highlight"  key={node.fields.slug}>{node.frontmatter.title}</Link>
+              <span className="text-sm text-grey"> - {node.frontmatter.date}</span>
+            </div>
+          )
+        })}
+      </div>
+      </Layout>
+    )
+  }
+}
+
+export default NotFound
+
+export const notFoundQuery = graphql`
+  query notFoundQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD.MM.YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
