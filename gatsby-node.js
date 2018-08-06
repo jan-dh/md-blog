44c8cb0b4f0d8ac3dacd8be699a98b77c0bedf47
Jan <jan@glue.be>
2018-08-06 15:20:12 +0200

    added trailing slashes

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,7 +58,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode, trailingSlash: false })
+    const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
       node,
